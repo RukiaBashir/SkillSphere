d43d5bfbd548983b30c97bfada36168f4320ded4
RukiaBashir <150848659+RukiaBashir@users.noreply.github.com>
2025-03-23 01:17:36 +0300

    Update settings.py

--- SkillSphere/settings.py
+++ SkillSphere/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'SkillSphere.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
+        default=os.getenv("postgresql://skillsphere_database_user:my9kd96GqJW7fFLIR0WEHXxPARKpAiJj@dpg-cvfd84jqf0us73fpbscg-a/skillsphere_database"),
         engine="django.db.backends.postgresql"  # Ensure PostgreSQL engine is set
     )
 }
