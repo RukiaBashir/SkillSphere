f2d7e8477c18d0c53502c71b7a3709c0dd982163
RukiaBashir <rukiabashir293@gmail.com>
2025-03-31 15:59:19 +0300

    Update settings.py

--- SkillSphere/settings.py
+++ SkillSphere/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY SETTINGS
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", default="").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
 
 DEBUG = os.getenv("DEBUG")
 
